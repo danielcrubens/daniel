a2e7e924abf8d2bb08bdba123caa0089147e04b3
danielcrubens <dcr.tecnologi@gmail.com>
2024-09-30 09:19:53 -0300

    refactor: image correção    :white_check_mark:

--- components/Front.vue
+++ components/Front.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center justify-center h-screen">
     <h1 class="text-5xl text-center font-bold text-white">Entusiasta por tecnologias front-end<br class="">Curioso em ux/ui3</h1>
-    <NuxtImg class="w-16 h-auto mx-auto" format="webp" src="/assets/image/code.webp" alt="Metrica Cokkin2u" loading="lazy" />
+    <img class="w-16 h-auto mx-auto" format="webp" src="/assets/image/code.webdp" alt="Metrica Cokkin2u" loading="lazy" />
   </div>
 </template>
 
